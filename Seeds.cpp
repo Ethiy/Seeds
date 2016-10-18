3dbece08f1ae900b3a0ad7f9623e3c92bc69f9e7
Ethiy <oussama.ennafii@ign.fr>
2016-10-18 10:47:46 +0200

    set up file

--- Seeds.cpp
+++ Seeds.cpp
@@ -2,13 +2,19 @@
 // Project:  Seeds
 // Author:   Pascal Monasse
 
+// ---------------------------- Imagine includes ----------------------------
 #include <Imagine/Graphics.h>
 #include <Imagine/Images.h>
+
+// ---------------------------- C++ includes ----------------------------
 #include <queue>
 #include <iostream>
+
+// ---------------------------- Namespaces ----------------------------
 using namespace Imagine;
 using namespace std;
 
+// ---------------------------- Constants ----------------------------
 /// Min and max disparities
 static const float dMin=-30, dMax=-7;
 
@@ -20,6 +26,12 @@ static const int win=(9-1)/2;
 /// To avoid division by 0 for constant patch
 static const float EPS=0.1f;
 
+/// 4-neighbors
+static const int dx[]={+1,  0, -1,  0};
+static const int dy[]={ 0, -1,  0, +1};
+
+
+// ---------------------------- Classes ----------------------------
 /// A seed
 struct Seed {
     Seed(int x0, int y0, int d0, float ncc0)
@@ -29,16 +41,24 @@ struct Seed {
 };
 
 /// Order by NCC
-bool operator<(const Seed& s1, const Seed& s2) {
+bool operator<(const Seed& s1, const Seed& s2)
+{
     return (s1.ncc<s2.ncc);
 }
 
-/// 4-neighbors
-static const int dx[]={+1,  0, -1,  0};
-static const int dy[]={ 0, -1,  0, +1};
+// ---------------------------- Functions ----------------------------
+
+void displayDisp(const Image<int>, Window, int);
+void show3D(const Image<Color>, const Image<int>);
+float correl(const Image<byte>&, int, int, float, const Image<byte>&, int, int, float);
+float sum(const Image<byte>&, int, int);
+float ccorrel(const Image<byte>&, int, int, const Image<byte>&, int, int);
+void find_seeds(Image<byte>, Image<byte>, float, Image<int>&, Image<bool>&, std::priority_queue<Seed>&);
+void propagate(Image<byte>, Image<byte>, Image<int>&, Image<bool>&, std::priority_queue<Seed>&);
 
 /// Display disparity map
-static void displayDisp(const Image<int> disp, Window W, int subW) {
+void displayDisp(const Image<int> disp, Window W, int subW)
+{
     Image<Color> im(disp.width(), disp.height());
     for(int j=0; j<disp.height(); j++)
         for(int i=0; i<disp.width(); i++) {
@@ -55,7 +75,8 @@ static void displayDisp(const Image<int> disp, Window W, int subW) {
 }
 
 /// Show 3D window
-static void show3D(const Image<Color> im, const Image<int> disp) {
+void show3D(const Image<Color> im, const Image<int> disp)
+{
 #ifdef IMAGINE_OPENGL // Imagine++ must have been built with OpenGL support...
     // Intrinsic parameters given by Middlebury website
     const float f=3740;
@@ -90,8 +111,7 @@ static void show3D(const Image<Color> im, const Image<int> disp) {
 
 /// Correlation between patches centered on (i1,j1) and (i2,j2). The values
 /// m1 or m2 are subtracted from each pixel value.
-static float correl(const Image<byte>& im1, int i1,int j1,float m1,
-                    const Image<byte>& im2, int i2,int j2,float m2)
+float correl(const Image<byte>& im1, int i1,int j1,float m1, const Image<byte>& im2, int i2,int j2,float m2)
 {
     float dist=0.0f;
     // ------------- TODO -------------
@@ -99,7 +119,7 @@ static float correl(const Image<byte>& im1, int i1,int j1,float m1,
 }
 
 /// Sum of pixel values in patch centered on (i,j).
-static float sum(const Image<byte>& im, int i, int j)
+float sum(const Image<byte>& im, int i, int j)
 {
     float s=0.0f;
     // ------------- TODO -------------
@@ -107,8 +127,7 @@ static float sum(const Image<byte>& im, int i, int j)
 }
 
 /// Centered correlation of patches of size 2*win+1.
-static float ccorrel(const Image<byte>& im1,int i1,int j1,
-                     const Image<byte>& im2,int i2,int j2)
+float ccorrel(const Image<byte>& im1,int i1,int j1, const Image<byte>& im2,int i2,int j2)
 {
     float m1 = sum(im1,i1,j1);
     float m2 = sum(im2,i2,j2);
@@ -118,48 +137,47 @@ static float ccorrel(const Image<byte>& im1,int i1,int j1,
 
 /// Compute disparity map from im1 to im2, but only at points where NCC is
 /// above nccSeed. Set to true the seeds and put them in Q.
-static void find_seeds(Image<byte> im1, Image<byte> im2,
-                       float nccSeed,
-                       Image<int>& disp, Image<bool>& seeds,
-                       std::priority_queue<Seed>& Q) {
+void find_seeds(Image<byte> im1, Image<byte> im2, float nccSeed, Image<int>& disp, Image<bool>& seeds, std::priority_queue<Seed>& Q)
+{
     disp.fill(dMin-1);
     seeds.fill(false);
     while(! Q.empty())
         Q.pop();
 
     for(int y=win; y+win<im1.height() && y+win<im2.height(); y++)
-        for(int x=win; x+win<im1.width(); x++) {
+        for(int x=win; x+win<im1.width(); x++)
+        {
             // ------------- TODO -------------
             // Hint: just ignore windows that are not fully in image
         }
 }
 
 /// Propagate seeds
-static void propagate(Image<byte> im1, Image<byte> im2,
-                      Image<int>& disp, Image<bool>& seeds,
-                      std::priority_queue<Seed>& Q) {
-    while(! Q.empty()) {
+void propagate(Image<byte> im1, Image<byte> im2, Image<int>& disp, Image<bool>& seeds, std::priority_queue<Seed>& Q)
+{
+    while(! Q.empty())
+    {
         Seed s=Q.top();
         Q.pop();
-        for(int i=0; i<4; i++) {
+        for(int i=0; i<4; i++)
+        {
             float x=s.x+dx[i], y=s.y+dy[i];
-            if(0<=x-win && 0<=y-win &&
-               x+win<im2.width() && y+win<im2.height() &&
-               ! seeds(x,y)) {
+            if(0<=x-win && 0<=y-win && x+win<im2.width() && y+win<im2.height() && ! seeds(x,y))
+            {
                 // ------------- TODO -------------
             }
         }
     }
 }
 
-int main()
+int main( int, char**)
 {
     // Load and display images
     Image<Color> I1, I2;
     if( ! load(I1, srcPath("im1.jpg")) ||
         ! load(I2, srcPath("im2.jpg")) ) {
         cerr<< "Unable to load images" << endl;
-        return 1;
+        return EXIT_FAILURE;
     }
     std::string names[5]={"image 1","image 2","dense","seeds","propagation"};
     Window W = openComplexWindow(I1.width(), I1.height(), "Seeds propagation",
@@ -189,5 +207,5 @@ int main()
     show3D(I1,disp);
 
     endGraphics();
-    return 0;
+    return EXIT_SUCCESS;
 }
